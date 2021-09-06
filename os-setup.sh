fc47dbb192a2fa050b71ad8d5e7cade7471a11fe
hebakk27 <henrik.bakkelund@norgesgruppen.no>
2021-09-06 13:10:51 +0200

    Added apt install zsh before installing oh-my-zsh. It did not work

--- os-setup.sh
+++ os-setup.sh
@@ -34,5 +34,6 @@ curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.c
 cp .vimrc ~/.vimrc
 
 # Oh-My-Zsh
+sudo apt install zsh;
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 cp .zshrc ~/.zshrc
